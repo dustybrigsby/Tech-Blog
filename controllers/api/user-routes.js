25ca12bd3b57ba5f029579b6bfa1304cec51c9e3
Dusty Brigsby <dustybrigsby@gmail.com>
2023-08-14 16:39:32 -0700

    Update user-routes.js
    Added login route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -59,6 +59,58 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+// Create new user
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create({
+            username: req.body.username,
+            password: req.body.password
+        });
 
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.username = userData.username;
+            req.session.loggedIn = true;
+            res.json(userData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
+});
+
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                username: req.body.username
+            }
+        });
+        if (!userData) {
+            res.status(400).json({ message: 'Username not Found' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect Password' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.username = userData.username;
+            req.session.loggedIn = true;
+            res.json({
+                user: userData,
+                message: 'You are now logged in!',
+            });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
+});
 
 module.exports = router;
