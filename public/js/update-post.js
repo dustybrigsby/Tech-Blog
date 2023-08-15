51b38f99995e1a72f7abc66bd1431200b51399d4
Dusty Brigsby <dustybrigsby@gmail.com>
2023-08-14 18:29:42 -0700

    Added post update javascript
    Added javascript for updating posts and the handlebars view for it

--- public/js/update-post.js
+++ public/js/update-post.js
@@ -1,11 +1,14 @@
-async function newPost(event) {
+async function updatePost(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value;
     const post_text = document.querySelector('textarea[name="post-text"]').value;
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
 
-    const response = await fetch(`/api/post`, {
-        method: 'POST',
+    const response = await fetch(`/api/post${id}`, {
+        method: 'PUT',
         body: JSON.stringify({
             title,
             post_text
@@ -22,4 +25,4 @@ async function newPost(event) {
     }
 }
 
-document.querySelector('.new-post-form').addEventListener('submit', newPost);
+document.querySelector('.update-post-form').addEventListener('submit', updatePost);
