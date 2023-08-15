e557c5201fff24e7b4793909b4c0d06f55f3b92e
Dusty Brigsby <dustybrigsby@gmail.com>
2023-08-14 18:45:16 -0700

    Added login.js

---
+++ public/js/login.js
@@ -0,0 +1,53 @@
+async function signupForm(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#signup-username').value.trim();
+    const password = document.querySelector('#signup-password').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        //check if the response status
+        if (response.ok) {
+            console.log('success');
+            alert('New user created you can now log in');
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+async function loginForm(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#login-username').value.trim();
+    const password = document.querySelector('#login-password').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupForm);
+document.querySelector('.login-form').addEventListener('submit', loginForm);
