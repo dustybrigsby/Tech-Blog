809b10e18697dfecd8f5110a86f0243a9437c660
Dusty Brigsby <dustybrigsby@gmail.com>
2023-08-14 17:39:22 -0700

    Update comment_routes.js
    Added code for comment routes

---
+++ controllers/api/comment_routes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+const Comment = require('../models');
+
+// Get all comments
+router.get('/', async (req, res) => {
+    try {
+        const commentData = await Comment.findAll();
+        res.json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Create new comment
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.create({
+            title: req.body.title,
+            post_text: req.body.post_text,
+            user_id: req.session.user_id
+        });
+        res.json(commentData);
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
+});
+
+// Delete comment by id
+router.delete('/:id', async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment found with this id.' });
+            return;
+        }
+        res.json(commentData);
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
+});
+
+module.exports = router;
